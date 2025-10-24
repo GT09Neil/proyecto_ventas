19b6ab20e65a7bf899756362f0cd242375bb71e2
SebasUQ <juans.gonzalezm@uqvirtual.edu.co>
2025-10-23 21:38:14 -0500

    actualizacion gitignore - requirements + excepcion db_connection

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,15 @@
 import pyodbc
 
 def get_connection():
-    conn = pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=localhost\SQLEXPRESS;"  # o NOMBRE_PC\SQLEXPRESS
-        "DATABASE=VentasElectrodomesticos;"
-        "Trusted_Connection=yes;"
-    )
-    return conn
+
+    try:
+        conn = pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};"
+            "SERVER=localhost\SQLEXPRESS;"  # o NOMBRE_PC\SQLEXPRESS
+            "DATABASE=VentasElectrodomesticos;"
+            "Trusted_Connection=yes;"
+        )
+        return conn
+    
+    except pyodbc.Error as e:
+        print("Error al conectar:", e)
